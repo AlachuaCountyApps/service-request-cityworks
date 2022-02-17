f48058ec96eca4b49def8a6324a04fb08288c395
Nishant Maurya <nish2493@rocketmail.com>
2022-02-17 15:04:14 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,65 +1,37 @@
-import { useState } from 'react';
-import { Grid, Typography } from '@mui/material';
+import { useEffect, useState } from "react";
+import { Grid, Typography } from "@mui/material";
 
-import Problem from '../components/Problem';
-import StyledDropdown from '../components/StyledDropdown';
-import Issue from '../components/Issue';
-import CallerQuestionsAnswers from '../components/CallerQuestionsAnswers';
-
-import problemAreaData from '../data/problemArea.json';
-import issuesData from '../data/issue.json';
-import questionsandAnswers from '../data/callerQuestions&Answers.json';
-import CallerInformation from '../components/CallerInformation';
-import IncidentInformation from '../components/IncidentInformation';
+import problemAreaData from "../data/problemArea.json";
+import questionsandAnswers from "../data/callerQuestions&Answers.json";
+import CallerInformation from "../components/CallerInformation";
+import IncidentInformation from "../components/IncidentInformation";
 
 export default function Home() {
-  const [domain, setDomain] = useState('');
-  const [problemAreas, setProblemAreas] = useState([]);
-  const [problemArea, setProblemArea] = useState('');
-  const [issues, setIssues] = useState([]);
-  const [issue, setIssue] = useState('');
+  const [domain, setDomain] = useState("");
+  const [problemArea, setProblemArea] = useState("");
+  const [issue, setIssue] = useState("");
   const [questionAnswers, setQuestionAnswers] = useState([]);
   const [selectedAnswers, setSelectedAnswers] = useState({});
-
-  const handleDomainChange = (e) => {
-    setDomain(e.target.value);
-    setProblemArea('');
-    setProblemAreas(problemAreaData[e.target.value]);
-  };
-
-  const handleProblemAreaChange = (e) => {
-    setProblemArea(e.target.value);
-    setIssue('');
-    setIssues(issuesData[e.target.value]);
-  };
 
   const handleIssueChange = (e, newVal) => {
     setSelectedAnswers({});
     if (newVal) {
       setIssue(newVal.label);
       setProblemArea(newVal.area);
+      for (const [key, value] of Object.entries(problemAreaData))
+        if (value.find((x) => x === newVal.area)) setDomain(key);
+
       if (
         questionsandAnswers[newVal.area] &&
         questionsandAnswers[newVal.area][[newVal.label]]
       )
         setQuestionAnswers(questionsandAnswers[newVal.area][newVal.label]);
     } else {
-      setIssue('');
-      setProblemArea('');
+      setIssue("");
+      setProblemArea("");
       setQuestionAnswers([]);
     }
   };
-
-  /*   const handleIssueChange = (e, newVal) => {
-    console.log(newVal);
-    setIssue(newVal.label);
-    setSelectedAnswers({});
-    if (
-      questionsandAnswers[problemArea] &&
-      questionsandAnswers[problemArea][[e.target.value]]
-    )
-      setQuestionAnswers(questionsandAnswers[problemArea][e.target.value]);
-  }; */
 
   const updateSelectedAnswers = (index, e) => {
     setSelectedAnswers((prevVal) => ({
@@ -68,9 +40,15 @@ export default function Home() {
     }));
   };
 
+  useEffect(() => {
+    console.log("domain:", domain);
+    console.log("problem area:", problemArea);
+    console.log("issue:", issue);
+  }, [domain, problemArea, issue]);
+
   return (
     <Grid container spacing={3}>
-      <Grid item xs={12} style={{ textAlign: 'center' }}>
+      <Grid item xs={12} style={{ textAlign: "center" }}>
         <Typography variant='h3'>New Service Request</Typography>
         <Typography variant='caption' display='block' color='red' gutterBottom>
           (See below for Emergency Contact Information)
@@ -86,25 +64,6 @@ export default function Home() {
           updateSelectedAnswers={updateSelectedAnswers}
         />
       </Grid>
-      <Grid item xs={12} sx={{ display: 'none' }}>
-        <StyledDropdown
-          id='domain'
-          title='Domain'
-          label='domain-label'
-          value={domain}
-          onChange={handleDomainChange}
-          data={['ACFD', 'Default', 'EPD']}
-        />
-      </Grid>
-      {domain && (
-        <Grid item xs={12} sx={{ display: 'none' }}>
-          <Problem
-            problemAreas={problemAreas}
-            problemArea={problemArea}
-            handleProblemAreaChange={handleProblemAreaChange}
-          />
-        </Grid>
-      )}
     </Grid>
   );
 }
